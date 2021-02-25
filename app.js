97bbeec0ae2cd16d508424696a8f111920f964a4
Anisha <agarwalanisha1111@gmail.com>
2021-02-25 22:09:16 +0530

    updated

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const User =require('./models/User');
 const MerchantRoute=require('./routes/MerchantRoute');
 const Merchant=require('./models/merchantModel')
 const TiffinRoute = require('./routes/TiffinRoute');
-
+const path = require('path')
+const shortid = require('shortid')
+const Razorpay = require('razorpay')
+const cors = require('cors')
 
 dotenv.config();
 connectDB();
@@ -23,6 +26,7 @@ var app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use(UserRoute);
 app.use(MerchantRoute);
@@ -138,7 +142,60 @@ passport.use(new facebookStrategy({
   }); 
 
 
-
+  const razorpay = new Razorpay({
+    key_id:process.env.KEY_ID,
+    key_secret:process.env.KEY_SECRET
+  })
+  
+  app.post('/verification', (req, res) => {
+    // do a validation
+    const secret = '12345678'
+  
+    console.log(req.body)
+  
+    const crypto = require('crypto')
+  
+    const shasum = crypto.createHmac('sha256', secret)
+    shasum.update(JSON.stringify(req.body))
+    const digest = shasum.digest('hex')
+  
+    console.log(digest, req.headers['x-razorpay-signature'])
+  
+    if (digest === req.headers['x-razorpay-signature']) {
+      console.log('request is legit')
+      // process it
+      require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+    } else {
+      // pass it
+    }
+    res.json({ status: 'ok' })
+  })
+  
+  app.post('/razorpay', async (req, res) => {
+    // const payment_capture = 1
+    const amount = 499
+    const currency = 'INR'
+  
+    const options = {
+      amount: amount * 100,
+      currency,
+      receipt: shortid.generate(),
+      payment_capture
+    }
+  
+    try {
+      const response = await razorpay.orders.create(options)
+      console.log(response)
+      res.json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  })
+  
 
 
 
