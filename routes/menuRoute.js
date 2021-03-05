2aefa4f8bc95bd70f113659504f82f8c2601c5cd
divyashasinha23 <divyashasinha23@gmail.com>
2021-03-05 18:28:19 +0530

    updated

--- routes/menuRoute.js
+++ routes/menuRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const menuController = require('../Controllers/MenuController');
 
-router.get('/menu',menuController.get_menu_tiffin);
+router.get('/menu/:id',menuController.get_menu_tiffin);
 router.post('/menu',menuController.post_menu_tiffin);
 
 module.exports = router;
