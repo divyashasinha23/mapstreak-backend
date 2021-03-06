1023091d4d14214401d0004e8e61404652b7edbb
divyashasinha23 <divyashasinha23@gmail.com>
2021-03-06 20:03:59 +0530

    updated

--- Controllers/MenuController.js
+++ Controllers/MenuController.js
@@ -32,3 +32,17 @@ const Menu = require('../models/menuModel');
       });
     }
   };
+
+  module.exports.get_menu_tiffin_by_id = async (req, res, next) => {
+    try {
+      const menu = await Menu.findOne({tiffinservice: req.params.id});
+      return res.status(200).json({
+        data: menu,
+      });
+    } catch (error) {
+      console.error(err);
+      res.status(500).json({
+        error: 'Server error',
+      });
+    }
+  };
