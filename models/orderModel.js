b86997c0b20647eeffd7e60767ac4396a27c14ab
divyashasinha23 <divyashasinha23@gmail.com>
2021-03-09 22:23:38 +0530

    updated order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,20 +3,24 @@ const mongoose = require('mongoose');
 const orderSchema =  new mongoose.Schema
 ({
     user: {
-      type: String,
-     
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
       ref: 'User',
     },
      tiffinservice: {
           type: String,
-         
+          required: true,
           ref: 'Tiffin',
         },
     deliveryAddress: {
-      address: { type: String,  },
-      city: { type: String},
-      postalCode: { type: String},
-      country: { type: String},
+      address: { type: String, required: true },
+      city: { type: String,required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    phone:{
+      type: Number,
+      required: true
     },
     paymentMethod: {
       type: String,
@@ -55,11 +59,11 @@ const orderSchema =  new mongoose.Schema
      
       default: false,
     },
-    deliveredAt: {
-      type: Date,
-    },
   
-});
+  
+},
+{timestamps: true}
+);
 
 
 const Order = mongoose.model('Order', orderSchema)
