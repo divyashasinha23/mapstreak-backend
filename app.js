fe21c611f958535da86529bcda36daf399e63d71
divyashasinha23 <divyashasinha23@gmail.com>
2021-03-30 18:05:58 +0530

    updated

--- app.js
+++ app.js
@@ -173,59 +173,59 @@ passport.use(new facebookStrategy({
   }); 
 
 
-  const razorpay = new Razorpay({
-    key_id:process.env.KEY_ID,
-    key_secret:process.env.KEY_SECRET
-  })
+  // const razorpay = new Razorpay({
+  //   key_id:process.env.KEY_ID,
+  //   key_secret:process.env.KEY_SECRET
+  // })
   
-  app.post('/verification', (req, res) => {
-    // do a validation
-    const secret = 'secret12'
+  // app.post('/verification', (req, res) => {
+  //   // do a validation
+  //   const secret = 'secret12'
   
-    console.log(req.body)
+  //   console.log(req.body)
   
-    const crypto = require('crypto')
+  //   const crypto = require('crypto')
   
-    const shasum = crypto.createHmac('sha256', secret)
-    shasum.update(JSON.stringify(req.body))
-    const digest = shasum.digest('hex')
+  //   const shasum = crypto.createHmac('sha256', secret)
+  //   shasum.update(JSON.stringify(req.body))
+  //   const digest = shasum.digest('hex')
   
-    console.log(digest, req.headers['x-razorpay-signature'])
+  //   console.log(digest, req.headers['x-razorpay-signature'])
   
-    if (digest === req.headers['x-razorpay-signature']) {
-      console.log('request is legit')
-      // process it
-      require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
-    } else {
-      // pass it
-    }
-    res.json({ status: 'ok' })
-  })
+  //   if (digest === req.headers['x-razorpay-signature']) {
+  //     console.log('request is legit')
+  //     // process it
+  //     require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+  //   } else {
+  //     // pass it
+  //   }
+  //   res.json({ status: 'ok' })
+  // })
   
-  app.post('/razorpay', async (req, res) => {
-    // const payment_capture = 1
-    const amount = req.body
-    const currency = 'INR'
+  // app.post('/razorpay', async (req, res) => {
+  //   // const payment_capture = 1
+  //   const amount = req.body
+  //   const currency = 'INR'
   
-    const options = {
-      amount: amount * 100,
-      currency,
-      receipt: shortid.generate(),
-      payment_capture
-    }
+  //   const options = {
+  //     amount: amount * 100,
+  //     currency,
+  //     receipt: shortid.generate(),
+  //     payment_capture
+  //   }
   
-    try {
-      const response = await razorpay.orders.create(options)
-      console.log(response)
-      res.json({
-        id: response.id,
-        currency: response.currency,
-        amount: response.amount
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  })
+  //   try {
+  //     const response = await razorpay.orders.create(options)
+  //     console.log(response)
+  //     res.json({
+  //       id: response.id,
+  //       currency: response.currency,
+  //       amount: response.amount
+  //     })
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // })
   
 //socket
 // const io=require('socket.io')(server)
